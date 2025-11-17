3ba1a297c1ba318c35053103ee3e6094b1d087c2
JKG <jkg66512@gmail.com>
2025-11-17 18:55:58 +0000

    Finished Base Checker piece setup...

--- Algorithms/intermediate/checkers.py
+++ Algorithms/intermediate/checkers.py
@@ -1,4 +1,5 @@
-from VividText import VividText as vt  # type: ignore
+from VividText import VividText as vt
+from rich.console import Console
 
 class CheckerPiece:
     def __init__(self, position, team, color):
@@ -6,7 +7,10 @@ class CheckerPiece:
         self.team = team
         self.color = color
         self.is_king = False
-    
+
+    def __str__(self):
+        return self.team[0].title()
+
     def get_valid_moves(self, board) -> list[tuple[int, int, bool]]:
         moves = []
         if self.is_king:
@@ -15,9 +19,11 @@ class CheckerPiece:
                 (1, -1), (1, 1)
             ]
         else:
-            directions = [
-                (-1, -1), (-1, 1)
-            ]
+            if self.color == "red":
+                directions = [(1, -1), (1, 1)]
+            else:
+                directions = [(-1, -1), (-1, 1)]
+
         row, col = self.position
 
         for dr, dc in directions:
@@ -28,15 +34,12 @@ class CheckerPiece:
                 if next_square is None:
                     moves.append((new_row, new_col, False))
                 elif isinstance(next_square, CheckerPiece):
-                    if next_square.team == self.team:
-                        pass
-
-                    elif next_square.team != self.team:
-                        new_row, new_col = new_row + dr, new_col + dc
-                        if 0 <= new_row < 8 and 0 <= new_col < 8:
-                            next_square = board[new_row][new_col]
-                            if next_square is None:
-                                moves.append((new_row, new_col, True))
+                    if next_square.team != self.team:
+                        jump_row = new_row + dr
+                        jump_col = new_col + dc
+                        if 0 <= jump_row < 8 and 0 <= jump_col < 8:
+                            if board[jump_row][jump_col] is None:
+                                moves.append((jump_row, jump_col, True))
 
         return moves
 
@@ -45,30 +48,52 @@ class CheckerPiece:
 
 class CheckerBoard:
     def __init__(self):
-        self.vt = vt(bold = True, sleep = 0)
-    
+        self.console = Console()
+        self.console.style = "bold white"
+
     def _generate_board(self) -> list:
         board = []
-        
         for row in range(8):
             board_row = []
             for col in range(8):
                 if row < 3 and (row + col) % 2 == 1:
                     board_row.append(CheckerPiece((row, col), "R", "red"))
-                elif row >= 8 - 3 and (row + col) % 2 == 1:
+                elif row >= 5 and (row + col) % 2 == 1:
                     board_row.append(CheckerPiece((row, col), "B", "blue"))
                 else:
                     board_row.append(None)
             board.append(board_row)
-
         return board
 
-    def _display_board(self, board):
-        top_row = "abcdefgh"
-        
-        for row in board:
+    def display_board(self, board) -> None:
+        # slot_space = 3
+        top_row = "    A   B   C   D   E   F   G   H"
+        split_row = " +" + "----" * 8 + "-+"
+
+        self.console.print(top_row)
+        self.console.print(split_row)
+
+        for i, row in enumerate(board, 1):
+            self.console.print(f"{i} | ", end = '')
             for col in row:
-                square = ""
+                if isinstance(col, CheckerPiece):
+                    square = col
+                    self.console.print(f"[{square.color}]{square}[/]", end = ' | ')
+
+                else:
+                    square = " "
+                    self.console.print(square, end = ' | ')
+            self.console.print(i)
+
+        self.console.print(split_row)
+        self.console.print(top_row)
+
+    def move_piece(self, board, row, col, trow, tcol, kill=False):
+        if kill:
+            mid_row = (row + trow) // 2
+            mid_col = (col + tcol) // 2
+            board[mid_row][mid_col] = None
+        board[trow][tcol], board[row][col] = board[row][col], None
 
 class MiniMax:
     pass
@@ -82,7 +107,12 @@ class Main:
         self.boardClass = CheckerBoard()
         self.board = self.boardClass._generate_board()
 
-    def test(self):
-        pass
+    def main(self):
+        # DONE: Check Piece Double hopping (killing)
+        # TODO: Start asking user for input and moving check pieces
+        # TODO: Start forcing kills
+        while self.running:
+            self.running = False
 
-vt(bold = True, sleep = .03).typewriter("Hello World")
+if __name__ == "__main__":
+    main = Main()
